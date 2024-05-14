04ef3762f7bc0baae552f4b1666a741d22c07e8a
Eugene Khimchenko <131620207+SoEugeneSo@users.noreply.github.com>
2024-05-14 09:59:06 +0300

    Delete TorneyX directory

--- TorneyX/backendapp/src/main/java/com/TorneyX/backendapp/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.TorneyX.backendapp.service;
-
-import com.TorneyX.backendapp.dto.UserDto;
-import com.TorneyX.backendapp.model.User;
-//UserService
-public interface UserService { User save(UserDto userDto);}
